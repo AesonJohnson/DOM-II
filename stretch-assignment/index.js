5a745c5e7c9809c6c339f1a284061435db35529c
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-03 13:28:56 -0800

    aeson-johnson

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,6 +1,12 @@
-const navLinkHover = document.querySelectorAll('.nav-link');
-navLinkHover.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.color = "red";
-    })
-})
+// const navLink = document.querySelector('.nav-link');
+
+// navLink.style.color = 'red';
+
+// navLink.forEach(link => {
+//     link.addEventListener('mouseover', () => {
+//         link.style.color = "red";
+//     })
+// })
+
+const heading = document.querySelector('.logo-heading');
+secondHeading.textContent = "please work";

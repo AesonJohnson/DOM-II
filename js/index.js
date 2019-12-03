aa32c0f718d7de7199558c7a320c0402b6087b1c
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-12-03 13:59:46 -0800

    aeson-johnson

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+// mouseover
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink[0].addEventListener('mouseover', function(){
+    navLink[0].style.color = 'red';})
+navLink[1].addEventListener('mouseover', function(){
+    navLink[1].style.color = 'orange';})
+navLink[2].addEventListener('mouseover', function(){
+    navLink[2].style.color = 'gold';})
+navLink[3].addEventListener('mouseover', function(){
+    navLink[3].style.color = 'yellow';})
+
+const header = document.querySelector('.logo-heading');
